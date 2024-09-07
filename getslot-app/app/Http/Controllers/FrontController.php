b6269013deb1ea3b87603110104357f817d56d8c
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-09-07 23:10:19 +0700

    update

---
+++ getslot-app/app/Http/Controllers/FrontController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    //
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
+    }
+    public function index()
+    {
+        $data = $this->frontService->getFrontPageData();
+        return view('front.index', $data);
+    }
+}
