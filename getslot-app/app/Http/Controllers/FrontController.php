242066e9f8defdc7b7640df8cf4d4c8e252f6d79
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-09-09 01:41:49 +0700

    Update Controller, Validasi and Booking Transaction with route

--- getslot-app/app/Http/Controllers/FrontController.php
+++ getslot-app/app/Http/Controllers/FrontController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\FrontService;
+use App\Models\Ticket;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
@@ -17,6 +19,19 @@ class FrontController extends Controller
     public function index()
     {
         $data = $this->frontService->getFrontPageData();
-        return view('front.index', $data);
+        dd($data);
+        // return view('front.index', $data);
+    }
+
+    public function details(Ticket $ticket)
+    {
+        dd($ticket);
+        // return view('front.details', compact('ticket'));
+    }
+
+    public function category(Category $category)
+    {
+        dd($category);
+        // return view('front.category', compact('category'));
     }
 }
--- getslot-app/routes/web.php
+++ getslot-app/routes/web.php
@@ -6,15 +6,16 @@ use App\Http\Controllers\BookingController;
 
 Route::get('/', [FrontController::class, 'index'])
 ->name('front.index');
-Route::get('/browse{category:slug}', [FrontController::class, 'category'])
+Route::get('/browse/{category:slug}', [FrontController::class, 'category'])
 ->name('front.category');
-Route::get('/details{ticket:slug}', [FrontController::class, 'details'])
+Route::get('/details/{ticket:slug}', [FrontController::class, 'details'])
 ->name('front.details');
 
 Route::get('/check-booking', [BookingController::class, 'checkBooking'])
 ->name('front.check_booking');
 Route::post('/check-booking/details', [BookingController::class, 'checkBookingDetails'])
 ->name('front.check_booking_details');
+
 Route::get('/booking/payment', [BookingController::class, 'payment'])
 ->name('front.payment');
 Route::post('/booking/payment', [BookingController::class, 'paymentStore'])
@@ -23,6 +24,7 @@ Route::get('/booking/{ticket:slug}', [BookingController::class, 'booking'])
 ->name('front.booking');
 Route::post('/booking/{ticket:slug}', [BookingController::class, 'bookingStore'])
 ->name('front.booking_store');
+
 Route::get('/booking/finished/{bookingTransaction}', [BookingController::class, 'bookingFinished'])
 ->name('front.booking_finished');
 
