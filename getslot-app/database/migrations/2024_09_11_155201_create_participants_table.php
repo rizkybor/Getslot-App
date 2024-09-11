41aafb43050ddc4729d5ebc034121f4bebc7262c
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-09-12 00:35:47 +0700

    Update-Migrations

---
+++ getslot-app/database/migrations/2024_09_11_155201_create_participants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_transaction_id')->constrained()->cascadeOnDelete();
+            $table->string('participant_name');
+            $table->string('identity_user');
+            $table->string('contingen');
+            $table->unsignedBigInteger('class_id');
+            $table->unsignedBigInteger('type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participants');
+    }
+};
---
+++ getslot-app/database/migrations/2024_09_11_170414_add_benefit_id_and_type_id_to_tickets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            //
+            $table->foreignId('type_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('benefit_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('like');
+            $table->date('event_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            // Hapus foreign key dan kolom terkait
+            $table->dropForeign(['type_id']);
+            $table->dropForeign(['benefit_id']);
+            $table->dropColumn(['type_id', 'benefit_id', 'like', 'event_date']);
+        });
+    }
+};
