9e0ffe27e528d8059a2cfbef0cb612aecea58201
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-09-13 17:57:32 +0700

    checkpoint-filament-create-new-booking
    configuration relation table Ticket, Type, Initial

--- getslot-app/app/Filament/Resources/BookingTransactionResource.php
+++ getslot-app/app/Filament/Resources/BookingTransactionResource.php
@@ -26,6 +26,9 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Repeater;
+use Illuminate\Support\Facades\Log;
+
 
 class BookingTransactionResource extends Resource
 {
@@ -54,8 +57,24 @@ class BookingTransactionResource extends Resource
                             ->required()
                             ->reactive()
                             ->afterStateUpdated(function ($state, callable $set) {
-                                $ticket = Ticket::find($state);
-                                $set('price', $ticket ? $ticket->price : 0);
+                                // $ticket = Ticket::find($state);
+                                // $set('price', $ticket ? $ticket->price : 0);
+                                
+
+                                // coba disini 
+                                $ticket = Ticket::with('type')->find($state);
+
+                                if ($ticket && $ticket->type->isNotEmpty()) {
+                                    $types = $ticket->type->pluck('name', 'id')->toArray();
+                                    $set('available_types', $types); // Menyimpan tipe yang tersedia ke state form
+                                } else {
+                                    $set('available_types', []); // Reset jika tidak ada tipe yang ditemukan
+                                }
+                        
+                                $set('selected_ticket_id', $state);
+                        
+                                // Debugging
+                                dump($state, $ticket->type);
                             }),
 
                         TextInput::make('total_participant')
@@ -72,48 +91,87 @@ class BookingTransactionResource extends Resource
                                 $set('total_amount', $totalAmount);
                             }),
 
-                        TextInput::make('total_amount')
+                        // TextInput::make('total_amount')
+                        //     ->required()
+                        //     ->numeric()
+                        //     ->prefix('IDR')
+                        //     ->readOnly()
+                        //     ->helperText('Harga sudah include PPN 11%')
+                    ]),
+
+                    Step::make('Customer Information')->schema([
+                        TextInput::make('name')
                             ->required()
-                            ->numeric()
-                            ->prefix('IDR')
-                            ->readOnly()
-                            ->helperText('Harga sudah include PPN 11%')
-                        ]),
+                            ->maxLength(255),
+                        TextInput::make('phone_number')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('email')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('booking_trx_id')
+                            ->required()
+                            ->maxLength(255),
+                    ]),
 
-                        Step::make('Customer Information')->schema([
-                            TextInput::make('name')
-                                ->required()
-                                ->maxLength(255),
-                            TextInput::make('phone_number')
-                                ->required()
-                                ->maxLength(255),
-                            TextInput::make('email')
-                                ->required()
-                                ->maxLength(255),
-                            TextInput::make('booking_trx_id')
-                                ->required()
-                                ->maxLength(255),
-                        ]),
+                    Step::make('Participant Information')->schema([
+                        Repeater::make('participants')
+                            ->schema([
+                                TextInput::make('participant_name')
+                                    ->label('Nama Peserta')
+                                    ->required()
+                                    ->maxLength(255),
 
-                        Step::make('Payment Information')->schema([
-                            ToggleButtons::make('is_paid')
+                                TextInput::make('identity_user')
+                                    ->label('Identitas Pengguna')
+                                    ->required()
+                                    ->maxLength(255),
+
+                                TextInput::make('contingen')
+                                    ->label('Kontingen')
+                                    ->required()
+                                    ->maxLength(255),
+
+                                Select::make('type_id')
+                                    ->label('Tipe Lomba')
+                                    ->options(function (callable $get) {
+                                        $ticketId = $get('selected_ticket_id');
+                                        logger($ticketId);
+                                        $ticket = Ticket::with('type')->find($ticketId);
+                    
+                                        if ($ticket && $ticket->type) {
+                                            return $ticket->type->pluck('name', 'id')->toArray();
+                                        }
+                    
+                                        return [];
+                                    })
+                                    ->required(),
+                    
+                            ])
+                            ->minItems(fn(callable $get) => is_numeric($get('total_participant')) ? (int) $get('total_participant') : 0)
+                            ->maxItems(fn(callable $get) => is_numeric($get('total_participant')) ? (int) $get('total_participant') : null) // Menangani nilai kosong
+                            ->columns(1)
+                    ]),
+
+                    Step::make('Payment Information')->schema([
+                        ToggleButtons::make('is_paid')
                             ->label('Apakah sudah membayar ?')
                             ->boolean()
                             ->grouped()
                             ->icons([
-                               'True' => 'heroicon-o-check',
-                               'False' => 'heroicon-o-x-mark',
+                                'True' => 'heroicon-o-check',
+                                'False' => 'heroicon-o-x-mark',
                             ])
                             ->required(),
-                           FileUpload::make('proof')
-                           ->image()
-                           ->required(),
-                           DatePicker::make('started_at')
-                           ->required(),
-                        ])
+                        FileUpload::make('proof')
+                            ->image()
+                            ->required(),
+                        DatePicker::make('started_at')
+                            ->required(),
+                    ])
                 ])
-                ->columnSpan('full')
-                ->columns(1)
+                    ->columnSpan('full')
+                    ->columns(1)
             ]);
     }
 
@@ -125,44 +183,44 @@ class BookingTransactionResource extends Resource
                 ImageColumn::make('ticket.thumbnail'),
 
                 TextColumn::make('name')
-                ->searchable(),
+                    ->searchable(),
 
                 TextColumn::make('booking_trx_id')
-                ->searchable(),
+                    ->searchable(),
 
                 IconColumn::make('is_paid')
-                ->boolean()
-                ->trueColor('success')
-                ->falseColor('danger')
-                ->trueIcon('heroicon-o-check-circle')
-                ->falseIcon('heroicon-o-x-circle')
-                ->label('Terverifikasi'),
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Terverifikasi'),
             ])
             ->filters([
                 //
                 SelectFilter::make('ticket_id')
-                ->label('Ticket')
-                ->relationship('ticket', 'name'),
+                    ->label('Ticket')
+                    ->relationship('ticket', 'name'),
             ])
             ->actions([
                 Tables\Actions\Action::make('approve')
-                ->label('Approve')
-                ->action(function (BookingTransaction $record) {
-                    $record->is_paid = true;
-                    $record->save();
+                    ->label('Approve')
+                    ->action(function (BookingTransaction $record) {
+                        $record->is_paid = true;
+                        $record->save();
 
-                    SendBookingApprovedEmail::dispatch($record);
+                        SendBookingApprovedEmail::dispatch($record);
 
-                    // Custom Notification
-                    Notification::make()
-                    ->title('Ticket Approved')
-                    ->success()
-                    ->body('The ticket has been successfully approved')
-                    ->send();
-                })
-                ->color('success')
-                ->requiresConfirmation()
-                ->visible(fn(BookingTransaction $record) => ! $record->is_paid),
+                        // Custom Notification
+                        Notification::make()
+                            ->title('Ticket Approved')
+                            ->success()
+                            ->body('The ticket has been successfully approved')
+                            ->send();
+                    })
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->visible(fn(BookingTransaction $record) => ! $record->is_paid),
 
                 Tables\Actions\ViewAction::make(),
 
