cc1bac51b02e34a573d8028c5184b6a4ec65c1cc
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-09-20 00:44:49 +0700

    update filesystem

--- getslot-app/config/filesystems.php
+++ getslot-app/config/filesystems.php
@@ -39,7 +39,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('ASSET_URL', null) . '/storage',
+            // 'url' => env('APP_URL').'/storage', // default
             'visibility' => 'public',
             'throw' => false,
         ],
